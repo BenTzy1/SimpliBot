15bf3c37f3ae66fac07398f46b91dd0546bedb34
Code`Lyphis <86455129+BenTzy1@users.noreply.github.com>
2024-03-31 21:23:08 +0800

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,23 +1,15 @@
-from aiogram import Bot, Dispatcher, executor, types
-import pyqrcode
+from flask import flask,render_template
+from threading import Thread
 
-bot = Bot(token='7019260893:AAEV8w6fJzvj4HHeOLtnCOywk-StGd20zr4')
-dp = Dispatcher(bot)
+app = Flask(_name_)
 
-@dp.message_handler(commands=['start', 'help'])
-async def welcome(message: types.Message):
-    await message.reply("Hello! Im Gunther Bot, Please follow my YT channel")
+@app_route('/')
+def index():
+    return'alive'
 
-@dp.message_handler(commands=['logo'])
-async def logo(message: types.Message):
-    await message.answer_photo('https://avatars.githubusercontent.com/u/62240649?v=4')
+def run():
+    app.run(host='0.0.0.0', port='8080')
 
-@dp.message_handler()
-async def qr(message: types.Message):
-    text = pyqrcode.create(message.text)
-    text.png('code.png', scale=5)
-    await bot.send_photo(chat_id=message.chat.id, photo=open('code.png', 'rb'))
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp)
+def keep_alive():
+    t = thread(target=run)
+    t.start()
